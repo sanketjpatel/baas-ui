478447a62490e83f5b2f17daf62aac2dc7ab0bb1
Sanket Patel <c.sanket.patel@uptake.com>
2017-11-14 16:33:12 -0600

    feat(table): add table on main page

--- src/app/asset-list/asset-list.component.ts
+++ src/app/asset-list/asset-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { MatTableDataSource, MatSort } from '@angular/material';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,14 +10,34 @@ import { Router } from '@angular/router';
 })
 export class AssetListComponent implements OnInit {
 
+  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  @ViewChild(MatSort) sort: MatSort;
+
   constructor(private router: Router) {}
 
   ngOnInit() {
+    this.dataSource.sort = this.sort;
   }
 
   onAddAsset() {
     this.router.navigate(['/add-asset']);
-
   }
-
 }
+
+
+export interface Element {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: Element[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'}
+];
