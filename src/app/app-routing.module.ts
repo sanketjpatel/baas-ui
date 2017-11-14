8db1a9391ebe4ca9be7b115f8abf6b6ce1c04113
Siddharth Soman <c.siddharth.soman@uptake.com>
2017-11-14 16:21:42 -0600

    feat(asset-details): add asset details page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AssetListComponent } from './asset-list/asset-list.component';
 import {AddAssetComponent} from './add-asset/add-asset.component';
+import {AssetDetailsComponent} from './asset-details/asset-details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/assets', pathMatch: 'full' },
   { path: 'assets', component: AssetListComponent },
-  { path: 'add-asset', component: AddAssetComponent }
+  { path: 'add-asset', component: AddAssetComponent },
+  { path: 'asset-details', component: AssetDetailsComponent }
 ];
 
 @NgModule({
--- src/app/asset-details/asset-details.component.ts
+++ src/app/asset-details/asset-details.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-asset-details',
   templateUrl: './asset-details.component.html',
-  styleUrls: ['./asset-details.component.css'],
+  styleUrls: ['./asset-details.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class AssetDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  onBackClick() {
+    this.router.navigate(['/assets']);
+  }
 }
